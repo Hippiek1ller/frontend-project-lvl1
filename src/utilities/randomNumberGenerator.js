5d32ddc13ddd219bb9c02970b802fa6122950b4c
Alexander <imak1232@gmail.com>
2020-01-05 15:48:10 +0300

    fixed eslint error

--- src/utilities/randomNumberGenerator.js
+++ src/utilities/randomNumberGenerator.js
@@ -1,1 +1,1 @@
-export default (maxNum, minNum = 0) => Math.floor(Math.random() * (maxNum - minNum +1)+minNum);
+export default (maxNum, minNum = 0) => Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
